f216a81dcad4dd6be6cd0705e776032c5d69c3f3
Florent Geron <florent.geron@orange.fr>
2022-03-20 23:13:39 +0100

    controllers created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'degustations/new'
+  get 'degustations/create'
+  get 'degustations/edit'
+  get 'degustations/delete'
+  get 'degustations/update'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
