1d2b7a6c24f1d0aa26966d220709c67af3cc48ec
Florent Geron <florent.geron@orange.fr>
2022-03-22 00:40:22 +0100

    created seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,183 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+puts "All right, folks! Let's seed!"
+
+puts "destroying all previous records"
+User.destroy_all
+Cave.destroy_all
+Appellation.destroy_all
+Cuvee.destroy_all
+Bouteille.destroy_all
+Degustation.destroy_all
+
+puts "creating a user"
+me = User.create!(
+  email: "master@bacchus.com",
+  password: "000000"
+)
+puts "created #{User.count}  #{'user'.pluralize(User.count)}"
+
+puts "creating 3 caves"
+orleans = Cave.create!(
+  nom: "Orleans",
+  localisation: "Orleans",
+  capacité: 200,
+  user: me
+)
+
+paris = Cave.create!(
+  nom: "Paris",
+  localisation: "Paris",
+  capacité: 180,
+  user: me
+)
+
+sologne = Cave.create!(
+  nom: "Chez Mamie",
+  localisation: "Selles Saint Denis",
+  capacité: 120,
+  user: me
+)
+
+puts "created #{User.count}  #{'user'.pluralize(User.count)}"
+
+puts "creating appellations from csv file"
+csv_file = "lib/assets/appellations.csv"
+csv_text = File.read(csv_file)
+csv = CSV.parse(csv_text, headers: true)
+csv.each do |row|
+  t = Appellation.new
+  t.pays = row['Pays']
+  t.region = row['Région']
+  t.nom = row['Nom']
+  t.couleur = row['Couleur']
+  t.norme = row['Norme']
+  t.save!
+end
+
+puts "created #{Appellation.count}  #{'appellation'.pluralize(Appellation.count)}"
+
+puts "creating cuvees now"
+
+cuvee1 = Cuvee.create!(
+    appelation_id: 24,
+    domaine: "Jean-Pierre Timbaud",
+    cuvee: "Cotes du Nord",
+    annee: Date.new(2011, 2, 5),
+    prix_achat: 14
+)
+
+cuvee2 = Cuvee.create!(
+  appelation_id: 44,
+  domaine: "Colonel Fabien",
+  cuvee: "Cotes du Sud",
+  annee: Date.new(2020, 2, 5),
+  prix_achat: 24
+)
+
+cuvee3 = Cuvee.create!(
+  appelation_id: 145,
+  domaine: "Saint Louis",
+  cuvee: "Cotes de l'Est",
+  annee: Date.new(2019, 2, 5),
+  prix_achat: 44
+)
+
+cuvee4 = Cuvee.create!(
+  appelation_id: 2845,
+  domaine: "Jules Verne",
+  cuvee: "Cotes de l'Ouest",
+  annee: Date.new(2017, 2, 5),
+  prix_achat: 25
+)
+
+puts "created #{Cuvee.count}  #{'cuvee'.pluralize(Cuvee.count)}"
+
+puts "creating Bouteilles now!"
+
+bouteille1 = Bouteille.create!(
+  cave: orleans,
+  cuvee: cuvee1,
+  date_achat: Date.new(2022, 1, 1)
+)
+
+bouteille2 = Bouteille.create!(
+  cave: orleans,
+  cuvee: cuvee1,
+  date_achat: Date.new(2022, 1, 1)
+)
+
+bouteille3 = Bouteille.create!(
+  cave: orleans,
+  cuvee: cuvee1,
+  date_achat: Date.new(2022, 1, 1)
+)
+
+bouteille4 = Bouteille.create!(
+  cave: paris,
+  cuvee: cuvee1,
+  date_achat: Date.new(2022, 1, 1)
+)
+
+bouteille5 = Bouteille.create!(
+  cave: paris,
+  cuvee: cuvee1,
+  date_achat: Date.new(2022, 1, 1)
+)
+
+bouteille6 = Bouteille.create!(
+  cave: paris,
+  cuvee: cuvee2,
+  date_achat: Date.new(2022, 1, 1)
+)
+
+bouteille7 = Bouteille.create!(
+  cave: paris,
+  cuvee: cuvee3,
+  date_achat: Date.new(2022, 1, 1)
+)
+
+bouteille8 = Bouteille.create!(
+  cave: sologne,
+  cuvee: cuvee3,
+  date_achat: Date.new(2022, 1, 1)
+)
+
+
+bouteille9 = Bouteille.create!(
+  cave: paris,
+  cuvee: cuvee4,
+  date_achat: Date.new(2022, 1, 1)
+)
+
+
+bouteille10 = Bouteille.create!(
+  cave: paris,
+  cuvee: cuvee4,
+  date_achat: Date.new(2022, 1, 1)
+)
+
+
+bouteille11 = Bouteille.create!(
+  cave: orleans,
+  cuvee: cuvee4,
+  date_achat: Date.new(2022, 1, 1)
+)
+
+puts "created #{Bouteille.count}  #{'bouteille'.pluralize(Bouteille.count)}"
+
+puts "creating degustations now"
+
+deg1 = Degustation.create!(
+  bouteille: bouteille1,
+  date_deg: Date.new(2022, 1, 4),
+  note_cuvee: 5,
+  commentaire: "Pas trop mal"
+)
+
+puts "created #{Degustation.count}  #{'degustation'.pluralize(Degustation.count)}"
+
+puts "That's all, folks! Happy Programming."
