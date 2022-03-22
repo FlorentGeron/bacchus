c1df9352f64e01ff4b770880363573c162cf9f8e
Florent Geron <florent.geron@orange.fr>
2022-03-22 12:22:56 +0100

    basic page waiting for filters

--- app/controllers/cuvees_controller.rb
+++ app/controllers/cuvees_controller.rb
@@ -1,2 +1,26 @@
 class CuveesController < ApplicationController
+
+def index
+@cuvees = Cuvee.all
+end
+
+def show
+@cuvee = Cuvee.find(params[:id])
+end
+
+def new
+end
+
+def create
+end
+
+def edit
+end
+
+def update
+end
+
+def delete
+end
+
 end
