28da6e3d5fa1f39a6f6d2b85af311da452a6ede9
Florent Geron <florent.geron@orange.fr>
2022-03-28 12:21:23 +0200

    links to deg and put-aside

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cuvees
   resources :caves
+  resources :cuvees
+  resources :bouteilles, except: %i[show update]
   resources :degustations, except: %i[new create]
-  resources :bouteilles, except: :show
-  resources :bouteilles, only: :show do
-    resources :degustation, only: %i[new create]
+  resources :cuvees, only: :show do
+    resources :bouteilles, only: :update
   end
-
+  resources :bouteilles, only: :show do
+    resources :degustations, only: %i[new create]
+  end
 end
