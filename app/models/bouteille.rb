8a455458d215288a8bd346c3900b0fd080607a6e
Florent Geron <florent.geron@orange.fr>
2022-04-06 18:32:35 +0200

    nok weekly stocks

--- app/models/bouteille.rb
+++ app/models/bouteille.rb
@@ -9,4 +9,26 @@ class Bouteille < ApplicationRecord
     self.statut = "à boire"
   end
 
+  def self.weekly_stock
+    source_for_chart = {
+      date: [],
+      stock: []
+    }
+    stock = Bouteille.all
+    count_achat = 0
+    count_degust = 0
+    stock.each do |bouteille|
+      count_achat = count_achat +1
+      source_for_chart[:date] << bouteille.date_achat
+      source_for_chart[:stock] << count_achat
+    end
+    degustations = Degustation.all
+    degustations.each do |degustation|
+      count_degust = count_degust -1
+      source_for_chart[:date] << degustation.date_deg
+      source_for_chart[:stock] << count_degust
+    end
+    source_for_chart
+  end
+
 end
