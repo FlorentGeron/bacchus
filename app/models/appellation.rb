952a7eeb72c190e9e51d7702bb5781db6aea1c58
Florent Geron <florent.geron@orange.fr>
2022-04-17 23:04:58 +0200

    constante couleurs

--- app/models/appellation.rb
+++ app/models/appellation.rb
@@ -1,7 +1,7 @@
 class Appellation < ApplicationRecord
   has_many :cuvees
   has_many :bouteilles, through: :cuvees
-  
+
   validates :pays, :region, :couleur, :norme, presence: true
   validates :nom, presence: true
 
@@ -9,6 +9,30 @@ class Appellation < ApplicationRecord
   require "nokogiri"
   require "csv"
 
+  REGIONCOLORS = {
+    Alsace: '#FAECDD',
+    Beaujolais: 'E5044B',
+    Bordeaux: '#67103C',
+    Bourgogne: '#EA4B7D',
+    Champagne: '#DCCCA3',
+    Jura: '#E6AD1C',
+    :'Languedoc-Roussillon' => '#4A2545',
+    Loire: '#FFB7C3',
+    :'Provence-Corse' => '#C4D6B0',
+    Rhône: '#0B7A99',
+    :'Savoie Bugey' => '#FFA552',
+    :'Sud-Ouest' => '#1CB9E6'
+  }
+
+  COULEURCOLORS = {
+    Blanc: '#FAECDD',
+    :'Blanc Moelleux' => '#E6AD1C',
+    Rouge: '#E5044B',
+    Rosé: '#FFB7C3',
+    :'Blanc Pétillant' => '#C4D6B0',
+    :'Rosé Pétillant' => '#EA4B7D',
+    :'Rouge Moelleux' => '#67103C'
+  }
 
   def self.scrap(url)
     appelation_list = []
