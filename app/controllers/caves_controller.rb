5e6c0d854732486ad4ed0796a41104dcbbb7f654
Florent Geron <florent.geron@orange.fr>
2022-04-18 15:05:13 +0200

    cave.new in controller

--- app/controllers/caves_controller.rb
+++ app/controllers/caves_controller.rb
@@ -1,10 +1,25 @@
 class CavesController < ApplicationController
 
-def new
-  @cave
-end
+  def new
+    @cave= Cave.new
+  end
 
-def create
-end
+  def create
+    @cave = Cave.new(cave_params)
+    @cave.user = current_user
+    if @cave.save
+      flash[:alert] = "Cave créée!"
+      redirect_to root_path
+    else
+      flash[:alert] = "Merci de revoir les informations"
+      render new
+    end
+  end
+
+  private
+
+  def cave_params
+    params.require(:cave).permit(:nom, :localisation, :capacité)
+  end
 
 end
