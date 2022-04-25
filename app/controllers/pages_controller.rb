cbcce7189e8dbfd99e16f953b1b2c7c1b3f49261
Florent Geron <florent.geron@orange.fr>
2022-04-26 00:28:20 +0200

    si pas cave, user passe par la création

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if user_signed_in?
+      homepath = current_user.caves.count.positive? ? cuvees_path : new_cave_path
+      redirect_to homepath
+    end
   end
 
   def settings
