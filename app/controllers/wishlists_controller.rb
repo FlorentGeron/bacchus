ae9c671de3ba405bc6ecccca18db2749b4e495d5
Florent Geron <florent.geron@orange.fr>
2022-07-27 12:37:30 +0200

    basic controller methods

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,2 +1,26 @@
 class WishlistsController < ApplicationController
+
+  def new
+    @wishlist = Wishlist.new
+  end
+
+  def create
+  end
+
+  def edit
+    @wishlist = Wishlist.find(params[:id])
+  end
+
+  def update
+  end
+
+  def show
+    @wishlist = Wishlist.find(params[:id])
+  end
+
+  def destroy
+    @wishlist = Wishlist.destroy(params[:id])
+  end
+
+
 end
