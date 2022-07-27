f532d93bb053297150419abedf3d197286ea7172
Florent Geron <florent.geron@orange.fr>
2022-07-27 12:59:05 +0200

    syntax

--- app/models/bouteille.rb
+++ app/models/bouteille.rb
@@ -5,9 +5,9 @@ class Bouteille < ApplicationRecord
   has_many :degustations
 
   validates :cuvee_id, :date_achat, :prix, presence: true
-  
-  validates :cave_id, presence: true, unless: wishlist_id
-  validates :wishlist_id, presence: true, unless: cave_id
+
+  validates :cave_id, presence: true, unless: :wishlist_id
+  validates :wishlist_id, presence: true, unless: :cave_id
 
   def extract
     self.statut = "à boire"
