aced0db17e3a0ea72938f0fa5bd3decdca132422
Florent Geron <florent.geron@orange.fr>
2022-07-27 12:09:44 +0200

    created model

---
+++ db/migrate/20220727100922_create_wishlists.rb
@@ -0,0 +1,11 @@
+class CreateWishlists < ActiveRecord::Migration[6.1]
+  def change
+    create_table :wishlists do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name
+      t.references :bouteille, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
